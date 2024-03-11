66222cd8a4372eaf4a564569d8223e36b1669879
ThisIsNotANamepng <jjhags@gmail.com>
2024-03-11 15:01:45 -0500

    Added Vigenere brute force and changed realEngine, Vigenere cipher now preserves spaces

--- Translator/README.md
+++ Translator/README.md
@@ -5,4 +5,12 @@ Translator is a tool which analyses an image and retrieves readable text in orde
 Abilities:
     - [ ] Read text from an image with OCR
     - [ ] Read Braille from an image
+    - [ ] Can interpret symbols in a code which aren't recognizable ASCII symbols 
     
+#### Symbol Interpreter
+
+A tool which is fed an image of symbols, usually a code which is encrypted as symbols (not standard english letters or numbers)
+
+The operator marks the corners of each symbol, and using the marks the tool can determine which are shapes. 
+
+It grayscales the image so colors won't strew the results. 
--- Translator/main.py
+++ Translator/main.py
@@ -3,12 +3,13 @@ This file is the user facing interface for the translator tool
 
 TODO:
     - Make thing
-    - Add an option to edit text after interpretting, so huamsn can make sure its right
+    - Add an option to edit text after interpretting, so humans can make sure its right
     - Only load the easyocr model when using ocr, maybe run it in a thread while the program asks the user for the filename
     - Maybe add a way to offload ocr to the cluster if the text is long and takes too much time on a laptop?
+    
 
 """""
 
-print("Translator loading...")
+print("Loading components...")
 import readImage
 
