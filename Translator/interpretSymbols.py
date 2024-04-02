307ab8b8a2b6e91b385d4f29135f416a7bf368c4
ThisIsNotANamepng <jjhags@gmail.com>
2024-04-02 20:51:18 +0000

    Improved cli tools

--- Translator/interpretSymbols.py
+++ Translator/interpretSymbols.py
@@ -3,27 +3,21 @@ This tool is meant to be able to accept an image of a group of symbols and recog
 
 """
 import sys
- 
-# Have to add Bletchley to the system path to access Bletchley tools
-sys.path.insert(0, 'Bletchley')
-
 import frequency
 
-def interpret():
+def interpret(text=""):
     """
     Takes a string representing non standard symbols
     """
 
     modes=["c", "p", "vbc", "vsbc"]
-    text=input("Symbols:  ")
-    mode=input("What mode of analysis do you want? 1] counts\n2] percentage\n3] verbose bar graph - count\n4] vervose simple bar graph - count\n")
+    if text=="":text=input("Symbols:  ")
+    mode=input("What mode of analysis do you want? \n1] counts\n2] percentage\n3] verbose bar graph - count\n4] vervose simple bar graph - count\n")
     try:
         mode=int(mode)
     except:
         print("Invalid mode inputted")
         exit()
-
-    
 
     if mode<3:
         mode=modes[mode-1]
@@ -31,5 +25,3 @@ def interpret():
     else:
         mode=modes[mode-1]
         frequency.frequencyAnalysis(text, mode)
-
-interpret()
